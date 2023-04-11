c03ea785d5a4aa147d25c184c53485ff3eba52f5
juanivalle <juanignaciovalle18@gmail.com>
2023-04-11 12:08:33 -0300

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def close_storage(exception):
     """Closes the storage engine on app context"""
