f1815f645a30a01fdd0d689e00693c3cc7f3214a
juanivalle <juanignaciovalle18@gmail.com>
2023-04-11 12:18:58 -0300

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,6 +12,17 @@ def status():
     return jsonify({'status': 'OK'})
 
 
+classes = {"amenities": "Amenity",
+           "cities": "City",
+           "places": "Place",
+           "reviews": "Review",
+           "states": "State",
+           "users": "User"}
+
 def get_stats():
     """comments"""
-    return jsonify(storage.count())
+    stats = {}
+    for name, cls in classes.items():
+        count = storage.count(cls)
+        stats[name] = count
+    return jsonify(stats)
