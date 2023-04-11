68e78c823687cea426c7af352355d4993b90e12e
juanivalle <juanignaciovalle18@gmail.com>
2023-04-11 11:54:11 -0300

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,14 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns status: OK"""
     return jsonify({'status': 'OK'})
+
+def get_stats():
+    """comments"""
+    return jsonify(storage.count())
